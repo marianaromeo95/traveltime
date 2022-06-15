2d25ca15fa0aaaba3952d16e1fe323c21f98abb3
Mariana Romeo <desenvolvedoramariromeo@gmail.com>
2022-06-15 14:41:20 -0300

    Primeiro commit

--- data/promocoes.js
+++ data/promocoes.js
@@ -5,9 +5,9 @@ const promocoes = [{
         pacote: ["passagem", "hospedagem"],
         hotel: "Atlantis, The Palm",
         images: [
-            "https://cf.bstatic.com/xdata/images/hotel/max1280x900/279644555.jpg?k=c83dbe9006b614743518f0243a93f6e08828ecd2b1fdc66f006780e288401e33&o=&hp=1",
-            "https://cf.bstatic.com/xdata/images/hotel/max1280x900/279636305.jpg?k=b4746e81544ab534b03c86a73cdd1202304e094c11468a2f6c60f0e5e59e4f66&o=&hp=1",
-            "https://cf.bstatic.com/xdata/images/hotel/max1280x900/266047314.jpg?k=5d451655f5e78e4608f5b4fef088ef8ed77190d3c5b4129367e4588d3da64090&o=&hp=1"
+            "https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZHViYWl8ZW58MHx8MHx8&w=1000&q=80",
+            "https://i.pinimg.com/originals/c8/74/46/c87446e4ac837673b1d6acca2a2bb96a.jpg",
+            "https://www.telegraph.co.uk/content/dam/luxury/2016/05/26/Burj_Al_Arab_Terrace_Infinity_Pool-xlarge.jpg"
         ],
         url: "https://www.decolar.com/pacotes/bsb/dxb/pacotes-para-dubai-desde-brasilia?flow=V-H&from=PSB&reSearch=true"
     },
@@ -18,8 +18,8 @@ const promocoes = [{
         pacote: ["passagem", "hospedagem"],
         hotel: "New York Central Park Hotel",
         images: [
-            "https://pix10.agoda.net/geo/city/318/1_318_02.jpg?ca=6&ce=1&s=1920x822",
             "https://i0.wp.com/blog.kennedyturismo.com.br/wp-content/uploads/2020/07/33-Central-Park.jpg?fit=2000%2C1333&ssl=1",
+            "https://upload.wikimedia.org/wikipedia/commons/2/2b/NYC_Downtown_Manhattan_Skyline_seen_from_Paulus_Hook_2019-12-20_IMG_7347_FRD_%28cropped%29.jpg",
             "https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2016/05/03/1430/Hyatt-Times-Square-P033-Times-Square.jpg/Hyatt-Times-Square-P033-Times-Square.16x9.jpg?imwidth=1920"
         ],
         url: "https://www.decolar.com/pacotes/bsb/nyc/pacotes-para-nova+york-desde-brasilia?flow=V-H&from=PSB"
@@ -31,8 +31,8 @@ const promocoes = [{
         pacote: ["passagem", "hospedagem"],
         hotel: "Eurostars Ibiza",
         images: [
+            "https://www.thepinnaclelist.com/wp-content/uploads/2016/01/01-Roca-Llisa-Luxury-Villa-Ibiza-Balearic-Islands-Spain.jpg",
             "https://mundo.top/wp-content/uploads/2020/10/shutterstock_639796432-1.jpg",
-            "https://www.melia.com/dam/jcr:613fbfbb-26e5-437c-adcb-8e1314a33f3e/224InnsideIbiza-Rooftop%20Pool%20Sunset-D.jpg",
             "https://images.trvl-media.com/hotels/25000000/24230000/24228800/24228721/2d1d36e1.jpg?impolicy=resizecrop&rw=1200&ra=fit"
         ],
         url: "https://www.decolar.com/pacotes/bsb/ibz/pacotes-para-ibiza-desde-brasilia?flow=V-H&from=PSB&reSearch=true"
