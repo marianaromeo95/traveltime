7fdd75694e99d35909c4aa16ec6b48c5bdb483d3
mariana romeo <marianaromeo95@gmail.com>
2022-06-20 21:26:33 -0300

    pacotes

---
+++ data/viagens2.js
@@ -0,0 +1,65 @@
+const viagens2 = [{
+        "id": 1,
+        "name": "Rio de Janeiro",
+        "ourPrice": "2100",
+        "airlinePrice": "27900",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/rio-rio-de-janeiro",
+        "discountPercentage": 21
+    },
+    {
+        "id": 100,
+        "name": "Porto Seguro",
+        "ourPrice": "40100",
+        "airlinePrice": "41100",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/byo-bonito",
+        "discountPercentage": 10
+    },
+    {
+        "id": 7,
+        "name": "Porto Alegre",
+        "ourPrice": "10900",
+        "airlinePrice": "25500",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/sao-sao-paulo",
+        "discountPercentage": 18
+    },
+    {
+        "id": 39,
+        "name": "Lençois maranhenses",
+        "ourPrice": "30000",
+        "airlinePrice": "31800",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/igu-foz-do-iguacu",
+        "discountPercentage": 0
+    },
+    {
+        "id": 25,
+        "name": "Pipa",
+        "ourPrice": "49300",
+        "airlinePrice": "22900",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/mcz-maceio",
+        "discountPercentage": 20
+    },
+    {
+        "id": 4,
+        "name": "Vitória",
+        "ourPrice": "27800",
+        "airlinePrice": "61900",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/ssa-salvador",
+        "discountPercentage": 60
+    },
+    {
+        "id": 21,
+        "name": "Fortaleza",
+        "ourPrice": "37100",
+        "airlinePrice": "54300",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/for-fortaleza",
+        "discountPercentage": 32
+    },
+    {
+        "id": 3,
+        "name": "Belo Horizonte",
+        "ourPrice": "22500",
+        "airlinePrice": "22100",
+        "url": "https://123milhas.com/melhores-destinos/passagens/brasilia/bhz-belo-horizonte-todos-os-aeroportos",
+        "discountPercentage": 62
+    }
+]
